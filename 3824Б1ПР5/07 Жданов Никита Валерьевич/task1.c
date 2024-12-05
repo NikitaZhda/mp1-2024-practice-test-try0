ff6beccc4e04897eb99709df9cc1e7979a6a27ee
NikitaZhda <mimimi19941994@gmail.com>
2024-12-05 11:02:07 +0300

    Update task1.c
    жданов

--- 3824Б1ПР5/07 Жданов Никита Валерьевич/task1.c
+++ 3824Б1ПР5/07 Жданов Никита Валерьевич/task1.c
@@ -1,7 +1,18 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <limits.h> // Для INT_MAX и INT_MIN
+
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  // Проверка на переполнение
+  if (a > 0 && b > 0 && a > INT_MAX - b) {
+    return INT_MAX; // Возвращаем INT_MAX при переполнении
+  }
+  // Проверка на недополнение
+  if (a < 0 && b < 0 && a < INT_MIN - b) {
+    return INT_MIN; // Возвращаем INT_MIN при недополнении
+  }
+  
+  return a + b; // Возвращаем сумму, если переполнения нет
 }
